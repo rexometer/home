fe3b5590c809c60b7942f0fae5d3d01ef5a3509e
Simsala <saile@gmx.de>
2017-06-15 17:51:42 +0200

    initial commit

---
+++ home_menu.php
@@ -0,0 +1,11 @@
+<?php  
+
+  $menu_right[] = array(
+        'name'=>'Menu',
+        'icon'=>'icon-home icon-white',
+        'path'=>"home",
+        'session'=>"write",
+        'order' => 60,
+        'divider' => true);
+
+   ?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+
+    global $session, $user, $path, $mysqli;
+    
+	//get API-Key for MyELectric
+    $apikey = "";
+    if ($session['write']) $apikey = "readkey=".$user->get_apikey_read($session['userid']);
+
+ //Dashboard-ID herausfinden
+	require "Modules/dashboard/dashboard_model.php";
+    $dashboard = new Dashboard($mysqli);
+  //Gibt das Array des Hauptdashboards zurueck. Mit $dash["id"] kann die ID ausgegeben werden.
+	$dash = $dashboard->get_main($session['userid']);
+?>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>RExometer - Startseite</title>
+<style type="text/css">
+body {
+	background-color: #bcbcbc;
+}
+</style>
+</head>
+
+<body>
+
+<p><center><img src="<?php echo $path; ?>Modules/home/images/Logo_RExometer_web_retina.png" width="266" height="80" /></center></p>
+<table width="100%" border="0" cellspacing="5" cellpadding="5">
+  <tr>
+    <td align="center" valign="middle"><p>
+    </p></td>
+    <td align="center" valign="middle"><br /></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top"><p><a href="<?php echo $path; ?>graph"><img src="<?php echo $path; ?>Modules/home/images/graph.png" width="276" height="214" /><br />
+    <?php echo _('Visualization'); ?></a></p>
+    <p>&nbsp;</p></td>
+    <td align="center" valign="top"><p><a href="<?php echo $path; ?>app/view?name=myelectric&<?php echo $apikey; ?>"><img src="<?php echo $path; ?>Modules/home/images/electric.png" alt="" width="276" height="214" /><br />
+    <?php echo _('Electricity consumption and costs'); ?></a></p>
+    <p>&nbsp;</p></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top"><a href="<?php echo $path; ?>dashboard/view?id=<?php echo $dash["id"]; ?>"><img src="<?php echo $path; ?>Modules/home/images/dashboard.png" alt="" width="276" height="214" /><br />
+    <?php echo _('User Dashboard'); ?></a></td>
+    <td align="center" valign="top"><a href="<?php echo $path; ?>settings"><img src="<?php echo $path; ?>Modules/home/images/einstellungen.png" alt="" width="276" height="214" /><br />
+    <?php echo _('Settings'); ?></a></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
